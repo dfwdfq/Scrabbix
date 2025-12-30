c6f50166a8f28265dec35edec107134d115cc9ab
uncle_joe <uncle_joe@kali.kali.com>
2025-12-30 09:09:15 +0300

    added procedure to handle keys

--- game.c
+++ game.c
@@ -1,11 +1,40 @@
 #include"game.h"
 
-
+void handle_keys(void)
+{
+  if(IsKeyReleased(KEY_A))
+    {
+      if(block_x > 0)
+	{
+	  char val = map[block_y][block_x];
+	  map[block_y][block_x] = '\0';
+	  map[block_y][--block_x] = val;
+	}
+    }
+  if(IsKeyReleased(KEY_D))
+    {
+      if(block_x < 7)
+	{
+	  char val = map[block_y][block_x];
+	  map[block_y][block_x] = '\0';
+	  map[block_y][++block_x] = val;
+	}
+    }
+  
+}
 void run_game(void)
 {
-
+  handle_keys();
+  
+  UPDATE_TIMER;
+  if(IS_DONE)
+    {
+      RESET_TIMER;
+      update_map();
+    }
 }
 void draw_game(void)
 {
   draw_borders();
+  draw_map();
 }
--- game.h
+++ game.h
@@ -1,6 +1,11 @@
 #ifndef GAME_H
 #define GAME_H
+#include<stddef.h>
+#include<stdio.h>
 #include"map.h"
+#include"timer.h"
+
+extern void handle_keys(void);
 
 extern void run_game(void);
 extern void draw_game(void);
