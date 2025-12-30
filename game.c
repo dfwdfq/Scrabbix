7fa765d2914af25b8e9320358348e3c6b3e4cc8a
uncle_joe <uncle_joe@kali.kali.com>
2025-12-30 09:50:01 +0300

    can't move blocks on ground

--- game.c
+++ game.c
@@ -2,7 +2,7 @@
 
 void handle_keys(void)
 {
-  if(IsKeyReleased(KEY_A))
+  if(IsKeyReleased(KEY_A) && block_y != -1)
     {
       if(block_x > 0)
 	{
@@ -11,7 +11,7 @@ void handle_keys(void)
 	  map[block_y][--block_x] = val;
 	}
     }
-  if(IsKeyReleased(KEY_D))
+  if(IsKeyReleased(KEY_D) && block_y != -1)
     {
       if(block_x < 7)
 	{
@@ -20,12 +20,21 @@ void handle_keys(void)
 	  map[block_y][++block_x] = val;
 	}
     }
-  
+  if(IsKeyReleased(KEY_S))
+    {
+      if(block_y < 13)
+	{
+	  char val = map[block_y][block_x];
+	  map[block_y][block_x] = '\0';
+	  map[++block_y][block_x] = val;
+	}
+    }
 }
 void run_game(void)
 {
   handle_keys();
-  
+
+  printf("%d %d\n",block_x,block_y);
   UPDATE_TIMER;
   if(IS_DONE)
     {
