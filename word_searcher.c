a40e13612e7d21e0254f50ea6e5a85eb13c372de
uncle_joe <uncle_joe@kali.kali.com>
2025-12-31 18:51:44 +0300

    implemented word searching in vertical and horizontal directions

---
+++ word_searcher.c
@@ -0,0 +1,94 @@
+#include"word_searcher.h"
+
+bool does_match(char* word)
+{
+  //just for now
+  //sure it's not good, because it's O(n), but I tried bsearch and it didn't work  
+  for(size_t n = 0;n<google_words_len;++n)
+    {
+      if(strcmp(word,google_words[n]) == 0)
+	return true;
+    }
+  return false;
+}
+void search(VertexListNode* head)
+{
+  VertexListNode* current = head;
+  
+  char word[8];
+  while(current != NULL)
+    {
+      //look in 4 direction, check word, if it matches, then destroy
+      
+      //search leftward
+      search_along_ox(-1,current->x,current->y,word);
+      if(does_match(word))
+	{
+
+	}
+      else
+	{
+	  printf("%s is not word!\n",word);
+	}
+      
+      //search rightward
+      search_along_ox(1,current->x,current->y,word);
+      if(does_match(word))
+	{
+
+	}
+      else
+	{
+	  printf("%s is not word!\n",word);
+	}
+
+      //search downward
+      search_along_oy(1,current->x,current->y,word);          
+      if(does_match(word))
+	{
+
+	}
+      else
+	{
+	  printf("%s is not word!\n",word);
+	}
+
+      //search upward
+      search_along_oy(-1,current->x,current->y,word);
+      if(does_match(word))
+	{
+
+	}
+      else
+	{
+	  printf("%s is not word!\n",word);
+	}
+
+      printf("found string:%s,len %d\n",word,strlen(word));
+      
+      current = current->next;
+    }
+
+}
+void search_along_ox(int dx,int x,int y,char *word)
+{
+  int cur = x;
+  int i = 0;
+  while(map[y][cur] != '\0')
+    {      
+      word[i++] = map[y][cur];
+      cur+=dx;
+      if(cur == 0 || cur == 7)break;
+    }
+}
+void search_along_oy(int dy,int x, int y,char *word)
+{
+  int cur = y;
+  int i = 0;
+  while(map[cur][x] != '\0')
+    {      
+      word[i++] = map[cur][x];
+      cur+=dy;
+      if(cur == 0 || cur == 7)break;
+    }
+}
---
+++ word_searcher.h
@@ -0,0 +1,18 @@
+#ifndef WORD_SEARCHER_H
+#define WORD_SEARCHER_H
+#include<string.h>
+#include<stdbool.h>
+#include<stdlib.h>
+#include"vertex_list.h"
+#include"map.h"
+#include"google_words.h"
+
+extern void search(VertexListNode* head);
+extern bool does_match(char* word);
+extern int str_cmp(const void* str1, const void* str2);
+
+extern void search_along_ox(int dx,int x,int y,char* word);
+extern void search_along_oy(int dy,int x, int y,char* word);
+
+
+#endif
