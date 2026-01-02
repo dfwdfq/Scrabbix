b710c2894d442d90b45ad827e99eaf80241d1024
uncle_joe <uncle_joe@kali.kali.com>
2026-01-02 03:33:28 +0300

    fixed reupdate procedure

--- game.c
+++ game.c
@@ -176,18 +176,36 @@ void erase_blocks(void)
 }
 void reupdate_blocks(void)
 {
-  VertexListNode* current = letters_head;
-  while(current != NULL)
+  bool moved;
+  
+  do {
+    moved = false;
+    VertexListNode* current = letters_head;
+
+    while(current != NULL)
     {
-      if(current->y != 13)
-	{
-	  if(map[current->y+1][current->x] == '\0')
-	    {
-	      current->y+=1;
-	      map[current->y][current->x] = map[current->y-1][current->x];
-	      map[current->y-1][current->x] = '\0';
-	    }
-	}
+      if(current->y < 13)
+      {
+        int new_y = current->y;
+        
+        while(new_y < 13 && map[new_y + 1][current->x] == '\0')
+        {
+          new_y++;
+        }
+        
+        if(new_y > current->y)
+        {
+          char letter = map[current->y][current->x];
+          
+          map[current->y][current->x] = '\0';
+          
+          current->y = new_y;        
+          map[current->y][current->x] = letter;
+          
+          moved = true;
+        }
+      }
       current = current->next;
     }
+  } while(moved);
 }
