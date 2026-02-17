c2a4ca03d278df5fba91b8f39bc5d4a0ee4b93dc
uncle_joe <uncle_joe@kali.kali.com>
2026-02-17 14:53:27 +0300

    rewrote algorithm of word removing. Iterate over array of found words and erase letter at position

--- erase_blocks.c
+++ erase_blocks.c
@@ -8,41 +8,15 @@ void erase_blocks(void)
       int y     = found_words_data[n].y;
       int len   = strlen(found_words[n]);
       score     += len*10;
-      
-      if(dir == RIGHT)
-	{
-	  for(int i = x;i<x+len;i++)
-	    {
-	      map[y][i] = '\0';
-	      letters_head = remove_by_value(letters_head,i,y);
-	    }
-	}
-      
-      if(dir == LEFT)
-	{
-	  for(int i = 0;i<len;++i)
-	    {
-	      map[y][x-i] = '\0';
-	      letters_head = remove_by_value(letters_head,x-i,y);
-	    }
-	}
-      
-      if(dir == UP)
-	{
-	  for(int i =0;i<len;++i)
-	    {
-	      map[y-i][x] = '\0';
-	      letters_head = remove_by_value(letters_head,x,y-i);
-	    }
-	}
 
-      if(dir == DOWN)
+      map[y][x] = '\0';
+      letters_head = remove_by_value(letters_head,x,y);
+      for(int i = 0;i<found_words_data[n].tail_len;++i)
 	{
-	  for(int i =y;i<y+len;++i)
-	    {
-	      map[i][x] = '\0';
-	      letters_head = remove_by_value(letters_head,x,i);
-	    }
+	  int _x = found_words_data[n].tail[i].x;
+	  int _y = found_words_data[n].tail[i].y;
+	  map[_y][_x] = '\0';
+	  letters_head = remove_by_value(letters_head,_x,_y);
 	}
     }
   found_words_counter = 0;
